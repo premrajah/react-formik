0eb3d956132f9d0af468992264af03a161bc88ec
Prem R <prem.rajah@gmail.com>
2021-04-20 11:12:19 +0200

    formik initialValues, onSubmit handler and Validate

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,19 +1,59 @@
 import React from 'react'
+import { useFormik } from 'formik';
+
+const initialValues = {
+    name: "",
+    email: "",
+    channel: "",
+}
+
+const onSubmit = values => {
+    console.log('form data ', values)
+}
+
+const validate = values => {
+    // returns an object, keys should match form field, value shoue be a string
+    let errors = {};
+
+    if (!values.name) {
+        errors.name = 'Required'
+    }
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Invalid email format"
+    }
+
+    if (!values.channel) {
+        errors.channel = 'Required'
+    }
+
+    return errors;
+}
 
 function YouTubeForm() {
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate,
+    });
+
+    // console.log('form values ', formik.values);
+
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} placeholder="Name" />
 
                 <label htmlFor="email">E-Mail</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} placeholder="E-Mail" />
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" />
+                <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} placeholder="Channel" />
 
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
