f0d51f78fd1840ce890b199e9af44ba04092e73e
Prem R <prem.rajah@gmail.com>
2021-04-20 11:40:09 +0200

    added validation and touched onBlur error check

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -40,20 +40,33 @@ function YouTubeForm() {
     });
 
     // console.log('form values ', formik.values);
+    // console.log('form errors ', formik.errors);
+    console.log('touched fields ', formik.touched);
 
     return (
         <div>
             <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} placeholder="Name" />
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} placeholder="Name" onBlur={formik.handleBlur} />
+                    {(formik.touched.name && formik.errors.name) && <div className="error">{formik.errors.name}</div>}
+                </div>
 
-                <label htmlFor="email">E-Mail</label>
-                <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} placeholder="E-Mail" />
+                <div className="form-control">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} placeholder="E-Mail" onBlur={formik.handleBlur} />
+                    {(formik.touched.email && formik.errors.email) && <div className="error">{formik.errors.email}</div>}
+                </div>
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} placeholder="Channel" />
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} placeholder="Channel" onBlur={formik.handleBlur} />
+                    {(formik.touched.channel && formik.errors.channel) && <div className="error">{formik.errors.channel}</div>}
+
+                </div>
 
                 <button type="submit">Submit</button>
+
             </form>
         </div>
     )
