3dbaccc4ce214a4c40fd59909ba551783d29ca6b
Prem R <prem.rajah@gmail.com>
2021-04-20 12:09:45 +0200

    refactored form with Formik, Form, Feild and ErrorMessage componenets

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup'; // object schema validation
 
 const initialValues = {
@@ -12,7 +12,6 @@ const onSubmit = values => {
     console.log('form data ', values)
 }
 
-
 const validationSchema = Yup.object({
     name: Yup.string().required('Required'),
     email: Yup.string().email('Invalid email').required('Required'),
@@ -21,42 +20,33 @@ const validationSchema = Yup.object({
 
 function YouTubeForm() {
 
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-    });
-
-    // console.log('form values ', formik.values);
-    // console.log('form errors ', formik.errors);
-    // console.log('touched fields ', formik.touched);
 
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            <Form>
                 <div className="form-control">
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Name" {...formik.getFieldProps('name')} />
-                    {(formik.touched.name && formik.errors.name) && <div className="error">{formik.errors.name}</div>}
+                    <Field type="text" id="name" name="name" placeholder="Name" />
+                    <ErrorMessage name="name" />
                 </div>
 
                 <div className="form-control">
                     <label htmlFor="email">E-Mail</label>
-                    <input type="email" id="email" name="email" placeholder="E-Mail" {...formik.getFieldProps('email')} />
-                    {(formik.touched.email && formik.errors.email) && <div className="error">{formik.errors.email}</div>}
+                    <Field type="email" id="email" name="email" placeholder="E-Mail" />
+                    <ErrorMessage name="email" />
                 </div>
 
                 <div className="form-control">
                     <label htmlFor="channel">Channel</label>
-                    <input type="text" id="channel" name="channel" placeholder="Channel" {...formik.getFieldProps('channel')} />
-                    {(formik.touched.channel && formik.errors.channel) && <div className="error">{formik.errors.channel}</div>}
+                    <Field type="text" id="channel" name="channel" placeholder="Channel" />
+                    <ErrorMessage name="channel" />
 
                 </div>
 
                 <button type="submit">Submit</button>
 
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
