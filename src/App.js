048ebec20a6e16d296780682e132cf122fe37cc2
Prem R <prem.rajah@gmail.com>
2021-04-20 10:17:00 +0200

    project init with simple form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import YouTubeForm from './components/YouTubeForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <YouTubeForm />
+        </div>
+    );
 }
 
 export default App;
